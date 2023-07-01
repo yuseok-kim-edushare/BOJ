835d74ec9c150114c3aa11cd8f1a0898cd348ab1
김유석(yuseok.kim) <yuseok.kim@edushare.kr>
2023-07-01 21:07:04 +0900

    초기 커밋.

---
+++ BOJ_C++/main.cpp
@@ -0,0 +1,7 @@
+﻿#include <cstdio>
+
+int main()
+{
+    printf("%s에 오신 것을 환영합니다!\n", "BOJ_C__");
+    return 0;
+}
---
+++ BOJ_C_sharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BOJ_C_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
