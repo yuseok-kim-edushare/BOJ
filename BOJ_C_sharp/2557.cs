10e68b606ed60133c04f289a305a99596a255d12
김유석 <yuseok.kim@edushare.kr>
2024-01-26 12:04:16 +0900

    Update and rename Program.cs to 2557.cs

--- BOJ_C_sharp/2557.cs
+++ BOJ_C_sharp/2557.cs
@@ -1,10 +1,10 @@
-﻿namespace BOJ_C_sharp
+namespace BOJ_C_sharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
