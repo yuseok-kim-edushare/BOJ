6050666f925df33ef68ec2fff873a0460a4f68f5
김유석(Yu Seok Kim) <yuseok.kim@edushare.kr>
2024-07-19 21:22:50 +0900

    Create 1018.py

---
+++ BOJ_python/1018.py
@@ -0,0 +1,24 @@
+n,m = map(int,input().split())
+table=[]
+test=[]
+for _ in range(n):
+    table.append(input())
+for i in range(n-7):
+    for j in range(m-7):
+        count1=0
+        count2=0
+        for a in range(i,i+8):
+            for b in range(j,j+8):
+                if (a+b)%2==0:
+                    if table[a][b] =='B':
+                        count1=count1+1
+                    else:
+                        count2=count2+1
+                else:
+                    if table[a][b] =='W':
+                        count1=count1+1
+                    else:
+                        count2=count2+1
+        test.append(count1)
+        test.append(count2)
+print(min(test))
