bac2c1d649ff94232e677aa1403ea2ef77571599
김유석(Yu Seok Kim) <yuseok.kim@edushare.kr>
2024-08-14 22:20:09 +0900

    Update stopwatch.py

--- BOJ_python/stopwatch.py
+++ BOJ_python/stopwatch.py
@@ -2,8 +2,14 @@ import sys
 import time
 import subprocess
 
+inputs = []
+rights = []
+
 
 number = input("어느 소스 실행할거야?")
 startTime = time.time()
-exec(open(number+".py").read())
+for i in len(inputs):
+    answer = solution(inputs[i])
+    if answer != rights[i]:
+        print("wrong ",i,'\n', rights[i],'\n',answer)
 print("excution time: %f"%(time.time() - startTime))
