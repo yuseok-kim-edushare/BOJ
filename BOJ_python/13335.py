d7e0c876b2b0531352b222b1e4f3dbf68ef07057
김유석(Yu Seok Kim) <yuseok.kim@edushare.kr>
2024-08-15 00:04:27 +0900

    Create 13335.py

---
+++ BOJ_python/13335.py
@@ -0,0 +1,19 @@
+n, bridge_length, can_holding_weight = map(int, input().split())
+trucks = list(map(int, input().split()))
+bridge=[]
+net_weights = 0
+answer = 1
+while trucks:
+    if net_weights + trucks[0] <= can_holding_weight:
+        entranceTruck = trucks.pop(0)
+        bridge.append([entranceTruck, bridge_length])
+        net_weights += entranceTruck
+    if bridge:
+        answer += 1
+        for i in range(len(bridge)):
+            bridge[i][1] -= 1
+        if bridge[0][1] == 0:
+            escapeTruck = bridge.pop(0)
+            net_weights -= escapeTruck[0]
+print(answer+bridge_length)
+        
